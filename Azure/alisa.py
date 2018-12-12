2e7a7fcb62d5a1b5f8291c04085283e0b6ef8e97
Stepan <stepchik2003@yandex.ru>
2018-12-13 01:41:49 +0300

    Added requirements and Azure/alisa.py

---
+++ Azure/alisa.py
@@ -0,0 +1,121 @@
+# coding: utf-8
+# Импортирует поддержку UTF-8.
+from __future__ import unicode_literals
+
+# Импортируем модули для работы с JSON и логами.
+import json
+import logging
+
+# Импортируем подмодули Flask для запуска веб-сервиса.
+from flask import Flask, request
+app = Flask(__name__)
+
+
+logging.basicConfig(level=logging.DEBUG)
+
+# Хранилище данных о сессиях.
+sessionStorage = {}
+
+# Задаем параметры приложения Flask.
+@app.route("/", methods=['POST'])
+
+def main():
+# Функция получает тело запроса и возвращает ответ.
+    logging.info('Request: %r', request.json)
+
+    response = {
+        "version": request.json['version'],
+        "session": request.json['session'],
+        "response": {
+            "end_session": False
+        }
+    }
+
+    handle_dialog(request.json, response)
+
+    logging.info('Response: %r', response)
+
+    return json.dumps(
+        response,
+        ensure_ascii=False,
+        indent=2
+    )
+
+
+# Функция для непосредственной обработки диалога.
+def handle_dialog(req, res):
+    user_id = req['session']['user_id']
+
+    if req['session']['new']:
+        # Это новый пользователь.
+        # Инициализируем сессию и поприветствуем его.
+
+        sessionStorage[user_id] = {
+            'suggests': [
+                "Выход",
+            ]
+        }
+
+        res['response']['text'] = 'Привет, друг!'
+        res['response']['buttons'] = get_suggests(user_id)
+        return
+
+    # Обрабатываем ответ пользователя.
+    if req['request']['original_utterance'].lower() in [
+        'выход'
+    ]:
+        # Пользователю надоело, прощаемся
+        res['response']['text'] = 'До свидания!'
+        return
+
+    # Если нет, то убеждаем его купить слона!
+    res['session']['original_utterance'] = req['request']['original_utterance'].lower()
+    res['response']['text'] = get_response(user_id)
+    res['response']['buttons'] = get_suggests(user_id)
+
+# Функция возвращает две подсказки для ответа.
+def get_suggests(user_id):
+    session = sessionStorage[user_id]
+
+    # Выбираем две первые подсказки из массива.
+    suggests = [
+        {'title': suggest, 'hide': True}
+        for suggest in session['suggests']
+    ]
+
+    # Убираем первую подсказку, чтобы подсказки менялись каждый раз.
+    session['suggests'] = session['suggests'][1:]
+    sessionStorage[user_id] = session
+
+    # Если осталась только одна подсказка, предлагаем подсказку
+    # со ссылкой на Яндекс.Маркет.
+    # поменял, чтобы всегда было False
+    if len(suggests) < 0:
+        suggests.append({
+            "title": "Ладно",
+            "url": "https://market.yandex.ru/search?text=слон",
+            "hide": True
+        })
+
+    return suggests
+
+
+from deeppavlov.skills.pattern_matching_skill import PatternMatchingSkill
+from deeppavlov.agents.default_agent.default_agent import DefaultAgent
+from deeppavlov.agents.processors.highest_confidence_selector import HighestConfidenceSelector
+
+hello = PatternMatchingSkill(responses=['Hello world!'], patterns=["hi", "hello", "good day"])
+bye = PatternMatchingSkill(['Goodbye world!', 'See you around'],
+                           patterns=["bye", "chao", "see you"])
+fallback = PatternMatchingSkill(["I don't understand, sorry", 'I can say "Hello world!"'])
+
+agent = DefaultAgent([hello, bye, fallback], skills_selector=HighestConfidenceSelector())
+
+
+def get_response(user_id):
+    session = sessionStorage[user_id]
+
+    # Получаем ответ от агента на основе запроса пользователя
+    responses = agent([session['original_utterance']])
+
+    return responses[0]
